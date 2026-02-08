4539aff75c362102f6d58bce78fd677f7ed21f09
Vlad <vlxdtimchenko@gmail.com>
2026-02-08 18:28:09 +0200

    chore: add favicons

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -38,10 +38,19 @@ export default defineNuxtConfig({
         { charset: 'utf-8' },
         { name: 'viewport', content: 'width=device-width, initial-scale=1' },
         { name: 'description', content: 'Vlad Timchenko • Software Engineer • Cloud-native .NET' },
-        { name: 'keywords', content: 'Vlad Timchenko, Vladyslav Timchenko, Software Engineer, .NET, backend' }
+        { name: 'keywords', content: 'Vlad Timchenko, Vladyslav Timchenko, Software Engineer, .NET, backend' },
+        { name: 'msapplication-TileColor', content: '#05090E' },
+        { name: 'msapplication-TileImage', content: '/mstile-150x150.png' },
+        { name: 'theme-color', content: '#05090E' }
       ],
       link: [
-        { rel: 'icon', href: '/favicon.ico' },
+        { rel: 'icon', href: '/favicon.ico', sizes: 'any' },
+        { rel: 'icon', type: 'image/svg+xml', href: '/logo.svg' },
+        { rel: 'icon', type: 'image/png', href: '/favicon-32x32.png', sizes: '32x32' },
+        { rel: 'icon', type: 'image/png', href: '/favicon-16x16.png', sizes: '16x16' },
+        { rel: 'apple-touch-icon', href: '/apple-touch-icon.png', sizes: '180x180' },
+        { rel: 'manifest', href: '/site.webmanifest' },
+        { rel: 'mask-icon', href: '/safari-pinned-tab.svg', color: '#18B7A4' },
         { rel: 'alternate', type: 'application/rss+xml', title: 'RSS Feed', href: '/rss.xml' },
         { rel: 'prefetch', href: '/feed' },
         { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
